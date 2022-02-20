3512dc5f74b3e80ebb0f43fd1df4fbad6f112e47
RobTov <tovelrob25@gmail.com>
2022-02-20 00:58:31 -0500

    t4

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
     const [ containerRef, isVisible ] = useIntersectionObserver({
         root: null, 
         rootMargin: "0px",
-        threshold: .6
+        threshold: .1
     })
 
     const {setObservedItem} = useContext(IntersectionContext);
